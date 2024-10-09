20ded674f8a7cdd88db56daa65e313a3380e323c
Gabinks <gabin.buignet@gmail.com>
2024-10-09 19:28:37 +0200

    Added redirection for link in Footer

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -12,12 +12,14 @@ export default function Footer() {
             <li><Link href={'/faq'}>F.A.Q</Link></li>
             <li><Link href={"/cgu"}>CGU</Link></li>
             <li><Link href={"/mentions-legales"}>Mentions légales</Link></li>
-            <li>baptiste.petit@psdoweb.com</li>
+            <li><Link href={"mailto:baptiste.petit@psdoweb.com"}>baptiste.petit@psdoweb.com</Link></li>
             <li>06-82-99-85-27</li>
             <div className={"flex items-center gap-10"}>
                 <Image src={"/images/socials_network/Facebook.png"} alt={"Logo Facebook"} width={40} height={40}/>
                 <Image src={"/images/socials_network/Instagram.png"} alt={"Logo Instagram"} width={40} height={40}/>
-                <Image src={"/images/socials_network/Linkedin.png"} alt={"Logo Linkedin"} width={40} height={40}/>
+                <Link href={"https://fr.linkedin.com/company/psdoweb"}>
+                    <Image src={"/images/socials_network/Linkedin.png"} alt={"Logo Linkedin"} width={40} height={40}/>
+                </Link>
             </div>
         </ul>
     </footer>
