76cd0201a24156a094f68867a7c5e65a893d6eb1
Gabinks <gabin.buignet@gmail.com>
2024-11-22 06:21:02 +0100

    Make some change for SkillsIcons components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import Equipe from "@/app/components/equipe";
 import PourquoiNous from "@/app/components/pourquoiNous";
 import Conception from "@/app/components/conception";
 import SkillsIcons from "@/app/components/SkillsIcons";
-import {generatePageMetadata} from "@/utils/metadata";
+import { generatePageMetadata } from "@/utils/metadata";
 
-export async function generateMetadata(){
+export async function generateMetadata() {
     return generatePageMetadata({
         title: "Accueil",
         description: "Transformez votre vision en réalité avec notre service de création de sites web personnalisés. Contactez-nous dès aujourd'hui pour discuter de votre projet !",
@@ -20,7 +20,7 @@ export default function Home() {
             <main
                 className={"flex flex-col gap-10 items-center"}>
                 <div className={"pt-36 lg:pt-28 flex flex-col md:items-start items-center z-30"}>
-                    <TextSlidin/>
+                    <TextSlidin />
                     <p className={"font-almarai font-bold text-3xl sm:text-4xl md:text-5xl xl:text-6xl"}>uniques et
                         sécurisés</p>
                 </div>
@@ -28,25 +28,25 @@ export default function Home() {
                     <p className={"md:w-[30vw] sm:w-[40vw] w-[65vw] px-2 text-center"}>{"Nous transformons vos idées en expériences numériques innovantes, capturant l'attention de votre public cible."}</p>
                 </div>
                 <div className={"grid grid-cols-2 sm:grid-cols-5 justify-items-center gap-20 p-24"}>
-                    <SkillsIcons source={"/images/icons/figma.png"} alt={"Figma"}/>
-                    <SkillsIcons source={"/images/icons/react.png"} alt={"React"}/>
-                    <SkillsIcons source={"/images/icons/tailwindcss.png"} alt={"TailwindCSS"}/>
-                    <SkillsIcons source={"/images/icons/nextjs.png"} alt={"NextJS"}/>
-                    <SkillsIcons source={"/images/icons/nodejs.png"} alt={"NodeJS"}/>
+                    <SkillsIcons source={"/images/icons/figma.png"} toShow={"Figma"} alt="Design responsive et création d'identité visuelle avec Figma" />
+                    <SkillsIcons source={"/images/icons/react.png"} toShow={"React"} alt="Développement d'applications web interactives avec React" />
+                    <SkillsIcons source={"/images/icons/tailwindcss.png"} toShow={"TailwindCSS"} alt="Création de sites web modernes et personnalisables avec TailwindCSS" />
+                    <SkillsIcons source={"/images/icons/nextjs.png"} toShow={"NextJS"} alt="Création de sites web performants et optimisés pour le SEO avec Next.js" />
+                    <SkillsIcons source={"/images/icons/nodejs.png"} toShow={"NodeJS"} alt="Développement backend robuste avec Node.js pour vos projets web" />
 
                     <div className={"col-span-1 hidden sm:block"}></div>
-                    <SkillsIcons source={"/images/icons/wordpress.png"} alt={"Wordpress"}/>
-                    <SkillsIcons source={"/images/icons/seo.png"} alt={"SEO"}/>
-                    <SkillsIcons source={"/images/icons/shopify.png"} alt={"Shopify"}/>
+                    <SkillsIcons source={"/images/icons/wordpress.png"} toShow={"Wordpress"} alt="Création de sites internet et gestion de contenu avec WordPress" />
+                    <SkillsIcons source={"/images/icons/seo.png"} toShow={"SEO"} alt="Optimisation du référencement naturel pour une meilleure visibilité en ligne" />
+                    <SkillsIcons source={"/images/icons/shopify.png"} toShow={"Shopify"} alt="Création de sites e-commerce performants avec Shopify" />
                     <div className={"col-span-1 hidden sm:block"}></div>
                 </div>
                 <div
                     className={"bg-[url('/images/utils/fleche.png')] bg-center bg-contain h-[2rem] sm:h-auto sm:bg-contain w-[80vw] min-w-56 max-w-72 sm:max-w-none sm:w-[40rem] py-2 flex justify-center items-center"}>
-                    <CustomButton text={"Nous contacter"}/>
+                    <CustomButton text={"Nous contacter"} />
                 </div>
-                <Equipe/>
-                <PourquoiNous/>
-                <Conception/>
+                <Equipe />
+                <PourquoiNous />
+                <Conception />
             </main>
         </>
     );
